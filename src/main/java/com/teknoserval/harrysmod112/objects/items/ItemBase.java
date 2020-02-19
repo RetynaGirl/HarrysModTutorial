8befdcdd182bbe90b58a2fb6df4f26193e7c6167
TeknoServal <teknoserval@gmail.com>
2020-02-18 18:37:35 -0700

    yepcopper ingots and copper blocks ya know

---
+++ src/main/java/com/teknoserval/harrysmod112/objects/items/ItemBase.java
@@ -0,0 +1,30 @@
+package com.teknoserval.harrysmod112.objects.items;
+
+import com.teknoserval.harrysmod112.HarrysMod112;
+import com.teknoserval.harrysmod112.init.ItemInit;
+import com.teknoserval.harrysmod112.proxy.ClientProxy;
+import com.teknoserval.harrysmod112.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemBase extends Item implements IHasModel{
+	
+	public ItemBase(String name) {
+		
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.MATERIALS);
+		
+		ItemInit.ITEMS.add(this);
+		
+	}
+
+	@Override
+	public void registerModels() {
+		
+		HarrysMod112.proxy.registerItemRenderer(this, 0, "inventory");
+		
+	}
+
+}
---
+++ src/main/java/com/teknoserval/harrysmod112/util/IHasModel.java
@@ -0,0 +1,7 @@
+package com.teknoserval.harrysmod112.util;
+
+public interface IHasModel {
+	
+	public void registerModels();
+
+}
