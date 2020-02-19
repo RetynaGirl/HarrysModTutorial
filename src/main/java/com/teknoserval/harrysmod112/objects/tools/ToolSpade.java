7a5861a8966796d58ad00527c2e002ab72d647af
TeknoServal <teknoserval@gmail.com>
2020-02-18 20:24:32 -0700

    adding the copper tools and armor

---
+++ src/main/java/com/teknoserval/harrysmod112/objects/tools/ToolSpade.java
@@ -0,0 +1,29 @@
+package com.teknoserval.harrysmod112.objects.tools;
+
+import com.teknoserval.harrysmod112.HarrysMod112;
+import com.teknoserval.harrysmod112.init.ItemInit;
+import com.teknoserval.harrysmod112.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemSpade;
+
+public class ToolSpade extends ItemSpade implements IHasModel{
+
+	public ToolSpade(String name, ToolMaterial material) {
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.TOOLS);
+		
+		ItemInit.ITEMS.add(this);
+	}
+
+	@Override
+	public void registerModels() {
+		
+		HarrysMod112.proxy.registerItemRenderer(this, 0, "inventory");
+		
+	}
+
+}
